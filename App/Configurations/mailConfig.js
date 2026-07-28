a265b14ea642b140064daa628271cf9759e16beb
Shivam-Software-Engineer <svmpathak1@gmail.com>
2026-07-28 06:59:55 +0530

    Nodemailer port change for test

--- App/Configurations/mailConfig.js
+++ App/Configurations/mailConfig.js
@@ -2,10 +2,7 @@ const nodemailer = require("nodemailer");
 
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-   port: 587,
-    secure: false,
-    family: 4, // use STARTTLS (upgrade connection to TLS after connecting)
+     service: "gmail",
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
